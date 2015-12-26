a8c63316c55a5d31ed860706f231b5bead97a7e2
TimohStudios <timogos.k@live.nl>
2015-12-27 00:37:36 +0100

    Gulpfile restructure

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,38 @@
-var gulp        = require('gulp');
-var uglify      = require('gulp-uglify');
-var sass        = require('gulp-ruby-sass');
-var browserSync = require('browser-sync');
-var imagemin    = require('gulp-imagemin');
+var gulp         = require('gulp');
+var uglify       = require('gulp-uglify');
+var sass         = require('gulp-ruby-sass');
+var browserSync  = require('browser-sync');
+var imagemin     = require('gulp-imagemin');
 var autoprefixer = require('gulp-autoprefixer');
-var cp = require('child_process');
+var cp           = require('child_process');
+
 
 
 var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
 /**
  * Build the Jekyll Site
  */
 gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
-        .on('close', done);
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+    .on('close', done);
 });
 
 /**
  * Rebuild Jekyll & do page reload
  */
 gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
+  browserSync.reload();
 });
-
-/**
- * Wait for jekyll-build, then launch the Server
- */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
-});
-
-function _errorLog(error){
-  console.error.bind(error);
-  $this.emit('end');
-}
-
 
 gulp.task('imagemin', function(){
   gulp.src('assets/img/*')
     .pipe(imagemin())
     .pipe(gulp.dest('assets/img/'));
 });
-
 
 //compile sass and export as compressed css
 gulp.task('sass', function(){
@@ -66,19 +49,28 @@ gulp.task('sass', function(){
 });
 
 //compress js and export to min folder
-gulp.task('scripts', function(){
+gulp.task('js', function(){
   gulp.src('assets/js/*.js')
     .on('error', _errorLog)
     .pipe(uglify())
-    .pipe(gulp.dest('assets/js/min'))
-    .pipe(browserSync.reload());
+    .pipe(gulp.dest('assets/js/min'));
 });
 
-//watch files for changes
-gulp.task('watch', function(){
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
+
   gulp.watch('assets/js/main.js',['scripts']);
   gulp.watch(['assets/css/sass/*.sass', 'assets/css/sass/*.scss'],['sass']);
   gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
-gulp.task('default', ['scripts', 'watch', 'sass', 'browser-sync']);
+gulp.task('default', ['js', 'watch', 'sass', 'browser-sync']);
+
+function _errorLog(error){
+  console.error.bind(error);
+  $this.emit('end');
+}
