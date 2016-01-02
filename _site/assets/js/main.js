c19699dbf7b4b0bb1c262c311c40200fd7037285
mike-griffin87 <michaelgriffin87@me.com>
2016-01-02 23:16:07 +0000

    Small changes to all files / data files added

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
 
-  console.log("ready");
+$(document).ready(function(){
 
   $('.about').on('click', function(){
     $('.about-container, .about-panel-overlay').addClass('show');
+    _currentWorkItems();
   });
 
   $('.close').on('click', function(){
@@ -15,15 +15,31 @@ $(document).ready(function(){
   });
 
 
-  //work section
 
   $('.work-item').on('click', function(){
     var _myWorkImage = ($(this).css('background-image'));
+    var _workItemText = ($(this).find('.work-item-text'));
+    var _CurrentWorkItem = $(this).attr('class');
+    var _next = $(_CurrentWorkItem).hide().next('.work-item');
 
-    $('.large-image-container').addClass('show-image');
-    $('.work-text-container').addClass('show-image');
+    $('.project-text').append(_workItemText.html());
+
+    $('.large-image-container').addClass('show-large-image');
+    $('.work-text-container').addClass('show-work-item-text');
     $('.large-image-container').css('background-image', _myWorkImage);
 
+    //Click on next project button
+      $('.next-button').on('click', function(){
+      console.log(_CurrentWorkItem);
+      });
+
+  });
+
+  //Close large image view and remove appended html
+  $('.close-work-large-container').on('click', function(){
+    $('.large-image-container').removeClass('show-large-image');
+    $('.work-text-container').removeClass('show-work-item-text');
+    $('.project-text').html("");
   });
 
 });
